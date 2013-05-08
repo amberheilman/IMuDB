fe23439d96cbdd5c7d3cd7655fa8ebec55ad2956
Stephen Slaughter <steve2slaughter@gmail.com>
2013-05-07 22:41:35 -0400

    added the settings we will need for development

--- IMuDB/settings.py
+++ IMuDB/settings.py
@@ -3,21 +3,23 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+
+import os.path
+PROJECT_DIR = os.path.dirname(__file__)
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Stephen W. Slaughter', 'steve2slaughter@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE':'django.db.backends.mysql',  # The following settings are not used with sqlite3:
+        'NAME': 'IMuDB',
+        'USER': 'root',
+        'PASSWORD': 'password',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
 
@@ -29,7 +31,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -53,15 +55,17 @@ USE_TZ = True
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
+# trailing slash
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
+
+LOGIN_REDIRECT_URL = '/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/media/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -108,6 +112,7 @@ ROOT_URLCONF = 'IMuDB.urls'
 WSGI_APPLICATION = 'IMuDB.wsgi.application'
 
 TEMPLATE_DIRS = (
+
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -120,6 +125,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'registration', #must install "sudo apt-get install python-django-registration" at the shell
+      
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
