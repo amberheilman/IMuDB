4108d88d1dba34432880049d1d24373cc73db599
Stephen Slaughter <steve2slaughter@gmail.com>
2013-05-25 09:05:06 -0400

    changed the STATIC_ROOT

--- IMuDB/IMuDB/settings.py
+++ IMuDB/IMuDB/settings.py
@@ -83,7 +83,7 @@ LOGIN_REDIRECT_URL = '/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'IMuDB/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
