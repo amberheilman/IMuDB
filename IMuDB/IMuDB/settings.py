c248c6ddf38a827492305a26dce8cb3b28859b3c
Stephen Slaughter <steve2slaughter@gmail.com>
2013-05-25 19:55:52 -0400

    ..

--- IMuDB/IMuDB/settings.py
+++ IMuDB/IMuDB/settings.py
@@ -194,4 +194,4 @@ LOGGING = {
 }
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL')
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')}
