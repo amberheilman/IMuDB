53ba7e7c07359e6017b63b628802d9171f7d7719
Stephen Slaughter <steve2slaughter@gmail.com>
2013-05-25 09:49:13 -0400

    ....

--- IMuDB/IMuDB/settings.py
+++ IMuDB/IMuDB/settings.py
@@ -156,7 +156,6 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     'discography',
     'gunicorn',
-    'storages', 
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
