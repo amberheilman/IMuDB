5bde2b38e56fb60bf0da2dd2e7b3bdd9aec59479
Stephen Slaughter <steve2slaughter@gmail.com>
2013-05-25 21:43:23 -0400

    changed settings back to normal

--- IMuDB/IMuDB/settings.py
+++ IMuDB/IMuDB/settings.py
@@ -15,9 +15,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#import dj_database_url
-#DATABASES['default'] = dj_database_url.config(default='postgres://user:pass@localhost/IMuDB')
-
 
 DATABASES = {
     'default': {
